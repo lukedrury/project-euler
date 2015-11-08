5cebc15a5b1cc5f3f87d4dafda9597bb3e3610ab
Luke Drury <lukecdrury@gmail.com>
2015-11-08 13:46:00 +0000

    New test cases

--- project-euler/multiples-of-3-and-5/Tests.cs
+++ project-euler/multiples-of-3-and-5/Tests.cs
@@ -5,7 +5,10 @@ namespace multiples_of_3_and_5
     [TestFixture]
     public class Tests
     {
+        [TestCase(10, 23)]
+        [TestCase(100, 2318)]
         [TestCase(1000, 233168)]
+        [TestCase(10000, 23331668)]
         public void GivesCorrectSum(int upperLimit, int expectedSum)
         {
             var multipleSumService = new MultipleSumService();
