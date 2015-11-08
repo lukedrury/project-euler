ddbb7719d54f707c8b702e4b0012dbdf6d10a485
Luke Drury <lukecdrury@gmail.com>
2015-11-08 13:55:16 +0000

    Even Fibonacci numbers.
    https://projecteuler.net/problem=2

---
+++ project-euler/even-fibonacci-numbers/EvenFibonacciSumService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace even_fibonacci_numbers
+{
+    public class EvenFibonacciSumService
+    {
+        public int CalculateSum(int upperLimit)
+        {
+            return 0;
+        }
+    }
+}
---
+++ project-euler/even-fibonacci-numbers/Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace even_fibonacci_numbers
+{
+    [TestFixture]
+    public class Tests
+    {
+        [TestCase(90, 231)]
+        public void GivesCorrectSum(int upperLimit, int expectedSum)
+        {
+            var evenFibonacciSumService = new EvenFibonacciSumService();
+
+            var sum = evenFibonacciSumService.CalculateSum(upperLimit);
+
+            Assert.That(sum, Is.EqualTo(expectedSum));
+        }
+    }
+}
