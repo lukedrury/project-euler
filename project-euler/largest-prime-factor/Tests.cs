1efc81aa6f0fadc08826ddeb504866fa3888ec9e
Luke Drury <lukecdrury@gmail.com>
2015-11-08 14:53:41 +0000

    Split test into two methods

--- project-euler/largest-prime-factor/Tests.cs
+++ project-euler/largest-prime-factor/Tests.cs
@@ -15,42 +15,51 @@ namespace largest_prime_factor
             Assert.That(largestPrimeFactor, Is.EqualTo(expectedLargestPrimeFactor));
         }
 
-        [TestCase(0, false)]
-        [TestCase(1, false)]
-        [TestCase(2, true)]
-        [TestCase(3, true)]
-        [TestCase(4, false)]
-        [TestCase(5, true)]
-        [TestCase(6, false)]
-        [TestCase(7, true)]
-        [TestCase(8, false)]
-        [TestCase(9, false)]
-        [TestCase(10, false)]
-        [TestCase(11, true)]
-        [TestCase(12, false)]
-        [TestCase(13, true)]
-        [TestCase(14, false)]
-        [TestCase(15, false)]
-        [TestCase(16, false)]
-        [TestCase(17, true)]
-        [TestCase(18, false)]
-        [TestCase(19, true)]
-        [TestCase(20, false)]
-        [TestCase(23, true)]
-        [TestCase(77, false)]
-        [TestCase(233, true)]
-        [TestCase(490, false)]
-        [TestCase(677, true)]
-        [TestCase(681, false)]
-        [TestCase(701, true)]
-        [TestCase(997, true)]
-        public void NumberKnowsIfItIsPrime(int value, bool expectedIsPrime)
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(5)]
+        [TestCase(7)]
+        [TestCase(11)]
+        [TestCase(13)]
+        [TestCase(17)]
+        [TestCase(19)]
+        [TestCase(23)]
+        [TestCase(233)]
+        [TestCase(677)]
+        [TestCase(701)]
+        [TestCase(997)]
+        public void NumberIsPrime(int value)
         {
             var number = new Number(value);
 
             var isPrime = number.IsPrime();
 
-            Assert.That(isPrime, Is.EqualTo(expectedIsPrime));
+            Assert.True(isPrime);
+        }
+
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(4)]
+        [TestCase(6)]
+        [TestCase(8)]
+        [TestCase(9)]
+        [TestCase(10)]
+        [TestCase(12)]
+        [TestCase(14)]
+        [TestCase(15)]
+        [TestCase(16)]
+        [TestCase(18)]
+        [TestCase(20)]
+        [TestCase(77)]
+        [TestCase(490)]
+        [TestCase(681)]
+        public void NumberIsNotPrime(int value)
+        {
+            var number = new Number(value);
+
+            var isPrime = number.IsPrime();
+
+            Assert.False(isPrime);
         }
     }
 }
