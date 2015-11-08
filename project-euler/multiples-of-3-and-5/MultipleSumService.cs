357d9f7df2a0c6c0e59895f645a07d550130e856
Luke Drury <lukecdrury@gmail.com>
2015-11-08 13:43:31 +0000

    Gives correct sum for 3/5 for upto 1000

---
+++ project-euler/multiples-of-3-and-5/MultipleSumService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiples_of_3_and_5
+{
+    public class MultipleSumService
+    {
+        public int CalculateSum()
+        {
+            var uniqueMultiples = new HashSet<int>();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                if (i%3 == 0 || i%5 == 0)
+                {
+                    uniqueMultiples.Add(i);
+                }
+            }
+
+            var sum = uniqueMultiples.Sum();
+            return sum;
+        }
+    }
+}
---
+++ project-euler/multiples-of-3-and-5/Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace multiples_of_3_and_5
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void GivesCorrectSum()
+        {
+            var multipleSumService = new MultipleSumService();
+
+            var sum = multipleSumService.CalculateSum();
+
+            Assert.That(sum, Is.EqualTo(233168));
+        }
+    }
+}
