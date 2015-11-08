8ee0db26810c22abdb843f02e92ee853488ab9e0
Luke Drury <lukecdrury@gmail.com>
2015-11-08 14:09:22 +0000

    Largest prime factor
    https://projecteuler.net/problem=3

---
+++ project-euler/largest-prime-factor/LargestPrimeFactorService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace largest_prime_factor
+{
+    public class LargestPrimeFactorService
+    {
+        public int CalculateLargestPrimeFactor(int target)
+        {
+            return -1;
+        }
+    }
+}
---
+++ project-euler/largest-prime-factor/Tests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace largest_prime_factor
+{
+    [TestFixture]
+    public class Tests
+    {
+        [TestCase(13195, 29)]
+        public void CorrectLargestPrimeFactor(int target, int expectedLargestPrimeFactor)
+        {
+            var service =  new LargestPrimeFactorService();
+
+            var largestPrimeFactor = service.CalculateLargestPrimeFactor(target);
+
+            Assert.That(largestPrimeFactor, Is.EqualTo(expectedLargestPrimeFactor));
+        }
+    }
+}
